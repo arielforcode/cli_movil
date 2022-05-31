4c2fc94e820635e79efbeed02b425b04342a8e58
acamargo <acamargo@axon.com.bo>
2022-05-31 14:12:02 -0400

    profile view

---
+++ lib/UI/screen/Profile/Utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import '../Model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+    'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+    name: 'Angel Ariel Camargo Mamani',
+    email: 'arielxime8@gmail.com',
+    about:
+    'me gusta programar mucho en lenguaje python y tambien en react para el desarrollo de frontends.',
+    isDarkMode: false,
+  );
+}
--- lib/UI/screen/account.dart
+++ lib/UI/screen/account.dart
@@ -1,18 +1,78 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class account extends StatefulWidget {
-  const account({Key? key}) : super(key: key);
+import '../screen/Profile/Model/user.dart';
+import '../screen/Profile/Utils/user_preferences.dart';
+import '../screen/Profile/Widget/appbar_widget.dart';
+import '../screen/Profile/Widget/button_widget.dart';
+import '../screen/Profile/Widget/numbers_widget.dart';
+import '../screen/Profile/Widget/profile_widget.dart';
 
+class account extends StatefulWidget {
   @override
-  State<account> createState() => _accountState();
+  _accountState createState() => _accountState();
 }
 
 class _accountState extends State<account> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Pantalla del perfil del usuario"),
+    final user = UserPreferences.myUser;
+
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 24),
+          buildName(user),
+          const SizedBox(height: 24),
+          Center(child: buildUpgradeButton()),
+          const SizedBox(height: 24),
+          NumbersWidget(),
+          const SizedBox(height: 48),
+          buildAbout(user),
+        ],
       ),
     );
   }
+
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.name,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      ),
+      const SizedBox(height: 4),
+      Text(
+        user.email,
+        style: TextStyle(color: Colors.grey),
+      )
+    ],
+  );
+
+  Widget buildUpgradeButton() => ButtonWidget(
+    text: 'Gestiona tu cuenta',
+    onClicked: () {},
+  );
+
+  Widget buildAbout(User user) => Container(
+    padding: EdgeInsets.symmetric(horizontal: 48),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Acerca de tu perfil',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 16),
+        Text(
+          user.about,
+          style: TextStyle(fontSize: 16, height: 1.4),
+        ),
+      ],
+    ),
+  );
 }
